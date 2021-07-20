3a6937c1fb0d07bd756761cee3de5f5dec61143b
hkl-iii <rahleggon@gmail.com>
2021-07-20 18:52:20 -0500

    added first if statement line 42 defcpp

--- def.cpp
+++ def.cpp
@@ -33,3 +33,24 @@ void introduction()
 
 
 }
+
+
+
+bool is_winner() 
+{ bool winner = false;
+  //checking the rows
+  if((board[0]==board[1]) && (board[1]==board[2]) && board[0] != " ")
+  {
+      winner = true;
+  }
+
+
+  //checking the columns
+
+
+
+  //checking the diagonals  
+
+
+
+}
