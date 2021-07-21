357b77ee51a405cd1854aae6bd3efb31b4b6528f
hkl-iii <rahleggon@gmail.com>
2021-07-20 19:14:53 -0500

    completed if statements

--- def.cpp
+++ def.cpp
@@ -75,11 +75,17 @@ bool is_winner()
   }
 
 
+ //checking the diagonals  
 
+ else if  ((board[0] == board[4]) && (board[4] == board[9])  && board[0] != " ")
+  {
+    winner = true;
+  }
 
+  else if ((board[2] == board[4]) && (board[4] == board[6]) && board[2] != " ")
+  {
+    winner = true;
+  }
 
-  //checking the diagonals  
-
-
-
+    return winner;
 }
